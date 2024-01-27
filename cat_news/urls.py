51bd981c25413e920e4c804f8e750d283bbf9bd7
ktay0557 <>
2024-01-27 13:05:32 +0000

    Add post_detail view and update styling

--- cat_news/urls.py
+++ cat_news/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.NewsList.as_view(), name='home'),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
--- cat_news/views.py
+++ cat_news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
@@ -9,3 +9,27 @@ class NewsList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "cat_news/index.html"
     paginate_by = 6
+
+
+def post_detail(request, slug):
+    """
+    Display an individual :model:`cat_news.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`cat_news.Post`.
+
+    **Template:**
+
+    :template:`cat_news/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "cat_news/post_detail.html",
+        {"post": post},
+    )
