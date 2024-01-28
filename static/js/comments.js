b5483fb46157461ef53e1ef0c3a90226ecf14e83
ktay0557 <>
2024-01-28 12:25:37 +0000

    Add delete function for user comments

--- static/js/comments.js
+++ static/js/comments.js
@@ -3,6 +3,10 @@ const commentText = document.getElementById("id_body");
 const commentForm = document.getElementById("commentForm");
 const submitButton = document.getElementById("submitButton");
 
+const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
+const deleteButtons = document.getElementsByClassName("btn-delete");
+const deleteConfirm = document.getElementById("deleteConfirm");
+
 /** 
  * Gives functionality to the edit button in comments section.
  * Allows user to edit their previous comments.
@@ -16,5 +20,18 @@ for (let button of editButtons) {
         commentText.value = commentContent;
         submitButton.innerText = "Update";
         commentForm.setAttribute("action", `edit_comment/${commentId}`);
+    });
+}
+
+/**
+ * Provides user with ability to delete comments.
+ * Alerts user to action to confirm deletion.
+ */
+
+for (let button of deleteButtons) {
+    button.addEventListener("click", (e) => {
+        let commentId = e.target.getAttribute("comment_id");
+        deleteConfirm.href = `delete_comment/${commentId}`;
+        deleteModal.show();
     });
 }
